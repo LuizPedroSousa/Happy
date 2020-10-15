08e9ea315436b3182542bf75b097fae9d6322849
Luiz Pedro <luizpedrosousa64@gmail.com>
2020-10-15 13:49:51 -0300

    feat(orphanagesController): added validation errors

--- backend/src/Controllers/orphanagesController.ts
+++ backend/src/Controllers/orphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanages from '../Models/Orphanages';
 import OrphanagesView from '../Views/Orphanages_view';
+import * as Yup from 'yup';
 
 export default class orphanagesController {
 
@@ -14,7 +15,7 @@ export default class orphanagesController {
             return res.status(400).json({
                 error: 'Orphanage not found',
             });
-            
+
         return res.status(201).json({
             Status: 'Okay',
             Orphanages: OrphanagesView.RenderMany(orphanage),
@@ -40,8 +41,7 @@ export default class orphanagesController {
             return { path: images.filename }
         })
 
-
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -50,19 +50,35 @@ export default class orphanagesController {
             opening_hours,
             open_on_weekends,
             images,
-        });
-        try {
-            await orphanagesRepository.save(orphanage);
-
-            return res.status(201).json({
-                Status: 'Okay, datas inserts to orphanage table',
-                orphanage,
-            });
-        } catch (error) {
-            return res.status(400).json({
-                error: 'Failed to insert datas in orphanage'
-            });
         }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required(),
+                })
+            )
+        });
+
+        await schema.validate(data, {
+            abortEarly: false,
+        });
+
+
+        const orphanage = orphanagesRepository.create(data);
+        await orphanagesRepository.save(orphanage);
+
+        return res.status(201).json({
+            Status: 'Okay, datas inserts to orphanage table',
+            orphanage,
+        });
     }
 
     async show(req: Request, res: Response) {
@@ -77,7 +93,7 @@ export default class orphanagesController {
                 error: 'Orphanage not found',
             });
 
-        
+
         return res.status(201).json({
             Status: 'Okay',
             orphanage: OrphanagesView.Render(orphanage),
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import 'express-async-errors';
 import { Router } from './routers';
 import path from 'path';
-import 'express-async-errors';
 import './Database/connection';
+import errorHandler from './Errors/handler';
+
 const App = express();
 
 App.use(express.json());
 App.use(Router);
 App.use('/Uploads', express.static(path.join(__dirname, '..', 'Uploads')))
+App.use(errorHandler)
+
 export { App };
 
